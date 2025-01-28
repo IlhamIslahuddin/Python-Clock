18e8572b22990a8528d654dbfc52333d6543b6b2
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2025-01-28 16:40:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,34 +15,42 @@ class MySmartClock:
         self.root.rowconfigure(0,weight=1)
         self.root.rowconfigure(1,weight=1)
         self.root.rowconfigure(2,weight=1)
+        # self.current_date = tk.Label(text=(datetime.date.today()).strftime("%d/%m/%Y"))
+        # self.current_date.grid(row=0,column=0,sticky="nw",padx=10,pady=10)
+        # self.TimerButton = tk.Button(self.root, text="Timer",command=self.Timer)
+        # self.StopwatchButton = tk.Button(self.root, text="Stopwatch",command=self.Stopwatch)
+        # self.CurrentTimeButton = tk.Button(self.root, text="Current Time",command=self.ShowCurrentTime)
+        # self.CurrentTimeButton.grid(row=2,column=0)
+        # self.StopwatchButton.grid(row=2,column=1)
+        # self.TimerButton.grid(row=2,column=2)
+        self.create_main_widgets()
+        self.screen = "CurrentTime"
+        self.detected_change = False
+        self.root.after(0,self.check_state)
+        
+        self.root.mainloop()
+        
+    def create_main_widgets(self):
+        self.current_date = tk.Label(text=(datetime.date.today()).strftime("%d/%m/%Y"),font=("Arial",20,"bold"))
+        self.current_date.grid(row=0,column=0,sticky="nw",padx=20,pady=20)
         self.TimerButton = tk.Button(self.root, text="Timer",command=self.Timer)
         self.StopwatchButton = tk.Button(self.root, text="Stopwatch",command=self.Stopwatch)
         self.CurrentTimeButton = tk.Button(self.root, text="Current Time",command=self.ShowCurrentTime)
         self.CurrentTimeButton.grid(row=2,column=0)
         self.StopwatchButton.grid(row=2,column=1)
         self.TimerButton.grid(row=2,column=2)
-        self.screen = "CurrentTime"
-        self.detected_change = False
-        self.root.after(0,self.check_state)
-        
-        self.root.mainloop()
 
     def check_state(self):
         if self.detected_change == True:
             for widget in self.root.winfo_children():
                 widget.destroy()
-            self.TimerButton = tk.Button(self.root, text="Timer",command=self.Timer)
-            self.StopwatchButton = tk.Button(self.root, text="Stopwatch",command=self.Stopwatch)
-            self.CurrentTimeButton = tk.Button(self.root, text="Current Time",command=self.ShowCurrentTime)
-            self.CurrentTimeButton.grid(row=2,column=0)
-            self.StopwatchButton.grid(row=2,column=1)
-            self.TimerButton.grid(row=2,column=2)
+            self.create_main_widgets()
                 
         if self.screen == "CurrentTime":
             currtime = datetime.datetime.now()
             currtime = currtime.strftime('%H:%M:%S')
             self.time_label = tk.Label(self.root,fg="orange",bg="black",text=currtime,font=("Arial",50,"bold"))
-            self.time_label.grid(row=1,column=1)
+            self.time_label.grid(row=1,column=1,sticky="nsew")
             
         elif self.screen == "Stopwatch":
             self.StartStopwatch()

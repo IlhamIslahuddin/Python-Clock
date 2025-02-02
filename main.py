70a21b0c3f9be0ad257363212fdae150f5c769d1
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2025-02-02 15:11:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import datetime
 from datetime import time
 import time as Time
@@ -12,9 +13,13 @@ class MySmartClock:
         self.root.columnconfigure(0,weight=1)
         self.root.columnconfigure(1,weight=1)
         self.root.columnconfigure(2,weight=1)
+        self.root.columnconfigure(3,weight=1)
+        self.root.columnconfigure(4,weight=1)
         self.root.rowconfigure(0,weight=1)
         self.root.rowconfigure(1,weight=1)
         self.root.rowconfigure(2,weight=1)
+        self.root.rowconfigure(3,weight=1)
+        self.root.rowconfigure(4,weight=1)
         self.create_main_widgets()
         self.screen = "CurrentTime"
         self.detected_change = False
@@ -25,13 +30,13 @@ class MySmartClock:
     def create_main_widgets(self):
         self.timer_num = time(0,0,0)
         self.current_date = tk.Label(text=(datetime.date.today()).strftime("%d/%m/%Y"),font=("Arial",20,"bold"))
-        self.current_date.grid(row=0,column=0,sticky="nw",padx=20,pady=20)
+        self.current_date.grid(row=0,column=0,columnspan=3,sticky="nw",padx=20,pady=20)
         self.TimerButton = tk.Button(self.root, text="Timer",command=self.Timer)
         self.StopwatchButton = tk.Button(self.root, text="Stopwatch",command=self.Stopwatch)
         self.CurrentTimeButton = tk.Button(self.root, text="Current Time",command=self.ShowCurrentTime)
-        self.CurrentTimeButton.grid(row=2,column=0)
-        self.StopwatchButton.grid(row=2,column=1)
-        self.TimerButton.grid(row=2,column=2)
+        self.CurrentTimeButton.grid(row=4,column=0)
+        self.StopwatchButton.grid(row=4,column=2)
+        self.TimerButton.grid(row=4,column=4)
 
     def check_state(self):
         if self.detected_change == True:
@@ -43,20 +48,24 @@ class MySmartClock:
             currtime = datetime.datetime.now()
             currtime = currtime.strftime('%H:%M:%S')
             self.time_label = tk.Label(self.root,fg="orange",bg="black",text=currtime,font=("Arial",50,"bold"))
-            self.time_label.grid(row=1,column=1,sticky="nsew")
+            self.time_label.grid(row=2,column=1,columnspan=3,sticky="nsew")
             
         elif self.screen == "Stopwatch":
             self.StartStopwatch()
             self.stopwatch = tk.Label(self.root,fg="orange",bg="black",text=self.newtime)
-            self.stopwatch.grid(row=1,column=1)
+            self.stopwatch.grid(row=2,column=1,columnspan=3,sticky="nsew")
             
         elif self.screen == "Timer":
             self.timer_label = tk.Label(self.root,fg="orange",bg="black",text=self.timer_num)
-            self.thirty_sec = tk.Button(text="30s",command=lambda: self.AddTime(30))
             self.start_timer = tk.Button(text="START",command=self.StartTimer)
-            self.timer_label.grid(row=1,column=1)
-            self.start_timer.grid(row=1,column=0)
-            self.thirty_sec.grid(row=1,column=2)
+            self.stop_timer = tk.Button(text="STOP")
+            self.thirty_sec = tk.Button(text="30s",command=lambda: self.AddTime(30))
+            self.one_min = tk.Button(text="1 min",command=lambda: self.AddTime(60))
+            self.timer_label.grid(row=2,column=1,columnspan=3,sticky="nsew")
+            self.start_timer.grid(row=2,column=0)
+            self.stop_timer.grid(row=2,column=4)
+            self.thirty_sec.grid(row=3,column=1)
+            self.one_min.grid(row=3,column=2)
             
         self.detected_change = False
         self.root.after(1000,self.check_state)
@@ -92,7 +101,7 @@ class MySmartClock:
     def StartTimer(self):
         total = self.timer_num.hour * 3600 + self.timer_num.minute * 60 + self.timer_num.second
         if total <= 0:
-            pass
+            messagebox.showinfo(message="Timer done!")
         else:
             if self.screen == "Timer":
                 total -= 1 #does not account for milliseconds yet
